e764c6d05d85f4eada98ad8f166f5c31d321f81a
diego <diego.guerrero5dg@outlook.com>
2025-08-28 22:24:13 -0400

    Agregar variables tipo char, string

---
+++ src/main/java/com/mycompany/fundamentosjava/variablesJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fundamentosjava;
+
+/**
+ *
+ * @author diego
+ */
+public class variablesJava {
+
+    public static void main(String args[]) {
+        //INT variable tipo entero 
+        int numero = 12;
+        numero = 13;//reasignación de variable, en java no se puede redeclarar
+        final double numero2 = Math.PI;//Variable constante 
+        
+        //----------------------------------------------------------------------
+        //CHAR variable tipo caracter
+        char textoLetra = 'a';
+        
+        //----------------------------------------------------------------------
+        //String variable tipo cadena de texto
+        String texto = "Saludos a todos";
+        
+    }
+
+}
