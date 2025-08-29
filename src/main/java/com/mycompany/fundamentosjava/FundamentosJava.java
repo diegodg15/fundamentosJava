fb94067199dc3e058f12dc939ae8cce93123b768
diego <diego.guerrero5dg@outlook.com>
2025-08-28 22:14:31 -0400

    Agregar operacion suma números

--- src/main/java/com/mycompany/fundamentosjava/FundamentosJava.java
+++ src/main/java/com/mycompany/fundamentosjava/FundamentosJava.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.fundamentosjava;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,19 @@ package com.mycompany.fundamentosjava;
 public class FundamentosJava {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+       
         System.out.println("Hola mundo");
+
+        System.out.println("Ingrese numero");
+        int numero1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo numero");
+        int numero2 = entrada.nextInt();
+
+        int sumaNumeros = numero1 + numero2;
+
+        System.out.println("La suma de ambos números es:" + "" + sumaNumeros);
+
     }
 }
