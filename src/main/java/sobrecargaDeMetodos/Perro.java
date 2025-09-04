f8385a36a7ff291b36b7065180a0a3ee98e717ab
diego <diego.guerrero5dg@outlook.com>
2025-09-03 23:24:25 -0400

    feat Agregar código ejemplo de: polimorfismo y sobrecarga de métodos.

---
+++ src/main/java/sobrecargaDeMetodos/Perro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sobrecargaDeMetodos;
+
+/**
+ *
+ * @author diego
+ */
+public abstract class Perro{
+    private int idAnimal;
+    private String descripcionAnimal;
+    //Constructores:
+    
+    //Métodos getters y setters:
+    
+    //Otros métodos
+    public abstract void hacerSonido();
+}
