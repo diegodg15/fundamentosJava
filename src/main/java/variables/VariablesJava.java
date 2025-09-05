fc16e8fc1f678c237b8817056b57a4ec7353ac6d
diego <diego.guerrero5dg@outlook.com>
2025-09-05 01:18:46 -0400

    Actualizar tipos de datos java.

---
+++ src/main/java/variables/VariablesJava.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author diego
+ */
+public class VariablesJava {
+
+    public static void main(String[] args) {
+        //----------------------------------------------------------------------
+        //Tipos de datos primitivos:
+
+        //variables de tipo texto Tipo de carácter
+        String textoString = "texto";//almacena texto, cadena de texto .
+
+        char textocaracter = 't';//almacena caracteres individuales. Los valores se escriben entre comillas simples.
+        //----------------------------------------------------------------------
+
+        // 1 byte = 8 bits
+        //variables de tipo números enteros Tipos enteros
+        byte numeroByte = 123;//almacena números pequeños (8 bits).
+
+        short numeroShort = 12345;//almacenanúmeros más grandes que un byte (16 bits).
+
+        int numeroInt = 2;//almacena numeros enteros.
+
+        long numeroLong = 1234567891011121314L;//almacena números enteros muy grandes, se utiliza con el sufijo L.
+        //----------------------------------------------------------------------
+
+        //variables de tipo números con decimales o racionales Tipos de punto flotante
+        float numeroFloat = 12.7f;//almacena números de punto flotante, con decimales, de presición simple (32 bits). utiliza el sufijo f.
+
+        double numeroDouble = 2.43;//almacena númeroscon decimales, de presición doble (64 bits).
+        //puede almacenar un número mucho más grande y con una mayor cantidad de decimales que float.
+        //----------------------------------------------------------------------
+
+        //almacena valor de tipo lógico o proposición verdadero o falso. Se utiliza el prefijo is.
+        boolean isBooleanTrue = true;
+        boolean isBooleanFalse = false;
+
+        //----------------------------------------------------------------------
+    }
+
+}
